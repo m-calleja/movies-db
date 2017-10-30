eb21dcb281d869e1841312055cd1fc34b90394a9
Mei <mei.calleja@gmail.com>
2017-10-30 08:45:57 +0100

    generated new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ItemComponent } from './item/item.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    ItemComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule

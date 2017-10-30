562c36d04f7d1396875c4f9a0c51d10c3c718585
Mei <mei.calleja@gmail.com>
2017-10-30 08:51:22 +0100

    tabs pop

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+  movies = [
+    { name: 'movie 1', side: '' },
+    { name: 'movie 2', side: '' }
+  ];
+  chosenList = 'all';
 
   constructor() { }
 
   ngOnInit() {
   }
 
-}
+  onChoose(side) {
+    this.chosenList = side;
+  }
+
